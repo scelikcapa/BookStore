8758f8babbe5453569ae1012ab5e7c2c839c76e4
scelikcapa <scelikcapa@outlook.com>
2022-06-25 08:35:20 +0300

    WebApi.UnitTest | CreateBooksCommandValidatorTests added

--- Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateCommand/CreateBookCommandTests.cs
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateCommand/CreateBookCommandTests.cs
@@ -34,4 +34,29 @@ public class CreateBookCommandTests : IClassFixture<CommonTestFixture>
                 .Invoking(() => command.Handle())
                 .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Kitap zaten mevcuttur.");
     }
+
+    [Fact]
+    public void WhenValidInputsAreGiven_Book_ShouldBeCreated()
+    {
+        // Arrange
+        CreateBookCommand command = new CreateBookCommand(context, mapper);
+        CreateBookModel model = new CreateBookModel(){
+            Title = "Hobbit",
+            PageCount = 0,
+            PublishDate = DateTime.Now.Date.AddYears(-10),
+            GenreId = 1};
+        command.Model = model;
+
+        // Act
+        FluentActions.Invoking(() => command.Handle()).Invoke();
+
+        // Assert
+        var book = context.Books.SingleOrDefault(b=> b.Title == model.Title);
+        
+        book.Should().NotBeNull();
+        book.PageCount.Should().Be(model.PageCount);
+        book.PublishDate.Should().Be(model.PublishDate);
+        book.GenreId.Should().Be(model.GenreId);
+
+    }
 }
---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateCommand/CreateBookCommandValidatorTests.cs
@@ -0,0 +1,70 @@
+using FluentAssertions;
+using WebApi.BookOperations.CreateBook;
+using WebApi.UnitTests.TestSetup;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.CreateCommand;
+
+public class CreateBookCommandValidatorTests : IClassFixture<CommonTestFixture>
+{
+    [Theory]
+    [InlineData("",1,1)]
+    [InlineData("Lord",0,1)]
+    [InlineData("Lord",1,0)]
+    public void WhenInvalidInputsAreGiven_Validator_ShouldBeReturnErrors(string title, int pageCount, int genreId)
+    {
+        // Arrange
+        CreateBookCommand command = new CreateBookCommand(null, null);
+        command.Model = new CreateBookModel{
+            Title = title,
+            PageCount = pageCount,
+            PublishDate = DateTime.Now.Date.AddYears(-1),
+            GenreId = genreId
+        };
+
+        // Act
+        CreateBookCommandValidator validator = new CreateBookCommandValidator();
+        var validationResult = validator.Validate(command);
+
+        // Assert
+        validationResult.Errors.Count.Should().BeGreaterThan(0);        
+    }
+
+    [Fact]
+    public void WhenDateTimeEqualNowIsGiven_Validator_ShouldReturnError()
+    {
+        // Arrange
+        CreateBookCommand command = new CreateBookCommand(null, null);
+        command.Model = new CreateBookModel{
+            Title = "Lord Of The Rings",
+            PageCount = 100,
+            PublishDate = DateTime.Now.Date,
+            GenreId = 1
+        };
+        // Act
+        CreateBookCommandValidator validator = new CreateBookCommandValidator();
+        var validationResult = validator.Validate(command);
+
+        // Assert
+        validationResult.Errors.Count.Should().BeGreaterThan(0);   
+    }
+
+    [Fact]
+    public void WhenValidInputsAreGiven_Validator_ShouldNotReturnError()
+    {
+        // Arrange
+        CreateBookCommand command = new CreateBookCommand(null, null);
+        command.Model = new CreateBookModel{
+            Title = "Lord Of The Rings",
+            PageCount = 100,
+            PublishDate = DateTime.Now.Date.AddDays(-1),
+            GenreId = 1
+        };
+        // Act
+        CreateBookCommandValidator validator = new CreateBookCommandValidator();
+        var validationResult = validator.Validate(command);
+
+        // Assert
+        validationResult.Errors.Count.Should().Be(0);   
+    }
+
+}
